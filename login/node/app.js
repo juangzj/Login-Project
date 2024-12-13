6f5a61f8e1b1e415d73e86b73ca33353217d444f
juanzgj <juanjosegoyeszambrano@gmail.com>
2024-12-13 17:40:07 -0500

    [added] script  to handle the app port named app.js

---
+++ login/node/app.js
@@ -0,0 +1,33 @@
+import express from 'express' // import express
+import detect from 'detect-port'// detect-port import
+import cors from 'cors' // cors import
+import db from './database/bd.js' // database import
+
+
+const DEFAULT_PORT = process.env.PORT || 5000; // handle the app port
+
+//database connection
+try {
+  await db.authenticate();
+  console.log('Succesful connection to database');
+} catch (error) {
+  console.log(`The connection error is: ${error}`);
+}
+
+
+detect(DEFAULT_PORT).then(port => {
+
+  const app = express() //express instance
+  app.use(cors)
+  app.use(express.json)
+
+
+
+
+  app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+
+}).catch(err => {
+  console.error(`Error detecting port: ${err.message}`);
+});
+
+
