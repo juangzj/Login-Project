7e064346328e3d1d116e4ace0138b2c6e5028f73
juanzgj <juanjosegoyeszambrano@gmail.com>
2024-12-24 15:15:03 -0500

    [feat] Show all users in frontend

--- login/react/login/src/components/userTable/UserTable.jsx
+++ login/react/login/src/components/userTable/UserTable.jsx
@@ -1,12 +1,32 @@
 import "react";
 import userTableStyles from './UserTable.module.css'; // Import the styles
+import axios from 'axios'
+import { useEffect, useState } from "react";
+
+const ShowUserBlogs = 'http://localhost:5000/auth/getAllUsers'
 
 const UserTable = () => {
-  const users = [
-    { id: 1, name: "Juan", lastName: "Pérez" },
-    { id: 2, name: "María", lastName: "Gómez" },
-    { id: 3, name: "Carlos", lastName: "López" },
-  ];
+
+  const [users, setUser] = useState([]);
+  const [error, setError] = useState(null);
+
+  // show users 
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  // function to show users
+  const getAllUsers = async () => {
+    try {
+      const res = await axios.get(ShowUserBlogs);
+      setUser(res.data);
+    } catch (error) {
+      setError('Error, could not show the user')
+      console.error(error);
+    }
+  }
+
+
 
   return (
     <div className={userTableStyles.pageContainer}>
@@ -18,15 +38,17 @@ const UserTable = () => {
               <th>ID</th>
               <th>Name</th>
               <th>Last Name</th>
+              <th>Email</th>
               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
+              <tr key={user.user_id}>
+                <td>{user.user_id}</td>
                 <td>{user.name}</td>
-                <td>{user.lastName}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
                 <td>
                   <button className={userTableStyles.editButton}>Edit</button>
                   <button className={userTableStyles.deleteButton}>Delete</button>
@@ -36,6 +58,7 @@ const UserTable = () => {
           </tbody>
         </table>
       </div>
+      {error && <div className='alert alert-danger'>{error}</div>} {/* Show errors */}
     </div>
   );
 };
